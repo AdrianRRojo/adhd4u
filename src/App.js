658a9f7329a53f1ca0d2c609e6d7c1a2d2e97014
Adrian Rojo <rojoadrian16@gmail.com>
2022-12-13 13:05:11 -0600

    fixed routing bug / created home file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom'
 
 
-
 import Forum from "./components/pages/Forum";
 import Planner from "./components/pages/Planner";
 import Tracker from "./components/pages/Tracker";
 import Resources from "./components/pages/Resources";
+import Home from "./components/pages/Home";
 
 function App() {
+
+  // Changes console.log to the variable "c"
+  const c = console.log.bind(document)
+
   return (
     <div className="App">
-      
+      <Routes>
+        <Route 
+          path='/'
+          element={
+            <Home />
+          }
+        />
+        <Route 
+          path='/forum'
+          element={
+            <Forum />
+          }
+        />
+        <Route 
+          path='/planner'
+          element={
+            <Planner />
+          }
+        />
+        <Route 
+          path='/tracker'
+          element={
+            <Tracker />
+          }
+        />
+        <Route 
+          path='/resources'
+          element={
+            <Resources />
+          }
+        />
+      </Routes>
     </div>
   );
 }
