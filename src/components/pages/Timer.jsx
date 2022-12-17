7e7340c66b169b7262a004299863dc7cd328f47d
Adrian Rojo <rojoadrian16@gmail.com>
2022-12-17 17:44:02 -0600

    created a simple stopwatch

--- src/components/pages/Timer.jsx
+++ src/components/pages/Timer.jsx
@@ -1,11 +1,22 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 
 
 export default function Timer(){
     const [seconds, setSeconds] = useState(0)
+    const [isRunning, setIsRunning] = useState(true)
+
+    useEffect(() => {
+    if(isRunning) {
+        const interval = setInterval(() => {
+            setSeconds(seconds => seconds + 1)
+        }, 1000)
+        return() => clearInterval(interval)
+    }
+        }, [isRunning])
     return(
         <div>
             <p>{seconds}, seconds</p>
+            <button className="bg-sky-500" onClick={() => setIsRunning(false)}>Stop Timer</button>
         </div>
     )
 }
