37844d7bc43eeb1cd380aa7b59fa9803085ff5de
Adrian Rojo <rojoadrian16@gmail.com>
2022-12-17 18:14:18 -0600

    created a simple alarm clock

--- src/components/pages/Timer.jsx
+++ src/components/pages/Timer.jsx
@@ -2,21 +2,57 @@ import React, {useEffect, useState} from "react"
 
 
 export default function Timer(){
-    const [seconds, setSeconds] = useState(0)
-    const [isRunning, setIsRunning] = useState(true)
+    // const [seconds, setSeconds] = useState(0)
+    // const [isRunning, setIsRunning] = useState(true)
 
-    useEffect(() => {
-    if(isRunning) {
-        const interval = setInterval(() => {
-            setSeconds(seconds => seconds + 1)
-        }, 1000)
-        return() => clearInterval(interval)
+    const [alarmTime, setAlarmTime] = useState(null)
+    const [inputTime, setInputTime] = useState('')
+
+    const handleChange = e => {
+        setInputTime(e.target.value)
     }
-        }, [isRunning])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const date = new Date()
+        date.setHours(inputTime.split(':')[0])
+        date.setMinutes(inputTime.split(':')[1])
+        setAlarmTime(date.toLocaleTimeString())
+    }
+    // useEffect(() => {
+    // if(isRunning) {
+    //     const interval = setInterval(() => {
+    //         setSeconds(seconds => seconds + 1)
+    //     }, 1000)
+    //     return() => clearInterval(interval)
+    // }
+    //     }, [isRunning])
     return(
         <div>
-            <p>{seconds}, seconds</p>
-            <button className="bg-sky-500" onClick={() => setIsRunning(false)}>Stop Timer</button>
+            {/*
+            <div> 
+                <p>{seconds}, seconds</p>
+                <button className="bg-sky-500" onClick={() => setIsRunning(false)}>Stop Timer</button> 
+            </div>
+            */}
+            <div>
+                {alarmTime ? (
+                    <p>Study for {alarmTime}</p>
+                ) : (
+                    <p>You're done!</p>
+                )}
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type='text'
+                        placeholder="HH:MM"
+                        value={inputTime}
+                        onChange={handleChange}
+                    >
+                    </input>
+                    <button type='submit'>Set alarm</button>
+                </form>
+            </div>
+            
         </div>
     )
 }
